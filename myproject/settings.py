42be7bc38dd97e9e1801b652083b0dfa0c35b830
Rohan Shah <rohanshah1306@gmail.com>
2020-05-15 11:04:20 +0530

    second

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xjw*$chf@_%5@dz&gg7%u_5ne!+wwg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','secure-garden-92806.herokuapp.com']
 
 
 # Application definition
