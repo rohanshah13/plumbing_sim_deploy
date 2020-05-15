c809a33f0b0e8539d62a34e2607049aab8e7ed31
Rohan Shah <rohanshah1306@gmail.com>
2020-05-15 17:39:56 +0530

    trial

--- myproject/settings.py
+++ myproject/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
-ASGI_APPLICATION = 'myproject.routing.application'
+ASGI_APPLICATION = 'myproject.routing.applicakhbtion'
 
 
 CHANNEL_LAYERS = {
