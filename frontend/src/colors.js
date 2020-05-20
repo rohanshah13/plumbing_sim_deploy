1f747edd3b671cd10af79db67ff34a5a91aa7a6e
Rohan Shah <rohanshah1306@gmail.com>
2020-05-20 19:09:41 +0530

    Fixed colors

--- frontend/src/colors.js
+++ frontend/src/colors.js
@@ -1,7 +1,20 @@
 let Colors = []
 Colors["blank"] = "white"
 Colors["active"] = "blue"
-Colors["pipe"] = "red"
+Colors["pipe_U_large"] = "linear-gradient(to right, white 5%,red 6%, white 96%)"
+Colors["pipe_D_large"] = "linear-gradient(to right, white 5%,red 6%, white 96%)"
+Colors["pipe_L_large"] = "linear-gradient(to bottom, white 5%,red 6%, white 96%)"
+Colors["pipe_R_large"] = "linear-gradient(to bottom, white 5%,red 6%, white 96%)"
+Colors["pipe_U_medium"] = "linear-gradient(to right, white 20%,red 21%, white 81%)"
+Colors["pipe_D_medium"] = "linear-gradient(to right, white 20%,red 21%, white 81%)"
+Colors["pipe_L_medium"] = "linear-gradient(to bottom, white 20%,red 21%, white 81%)"
+Colors["pipe_R_medium"] = "linear-gradient(to bottom, white 20%,red 21%, white 81%)"
+Colors["pipe_U_small"] = "linear-gradient(to right, white 33%,red 34%, white 67%)"
+Colors["pipe_D_small"] = "linear-gradient(to right, white 33%,red 34%, white 67%)"
+Colors["pipe_L_small"] = "linear-gradient(to bottom, white 33%,red 34%, white 67%)"
+Colors["pipe_R_small"] = "linear-gradient(to bottom, white 33%,red 34%, white 67%)"
 Colors["split"] = "black"
 
 export default Colors;
+
+//"linear-gradient(to right, white 20%,red 21%, white 81%)"
