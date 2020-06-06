0e343363f29da821dfd6964ec2f6a2ddd56db0fb
Rohan Shah <rohanshah1306@gmail.com>
2020-06-06 12:52:04 +0530

    a

---
+++ frontend/builda/precache-manifest.c1a2fc57d5e4718b7537ac728ad8a2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478e7fdb98c86a10c9252becbf3b6072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a542c064a8c8c7c7d5",
+    "url": "/static/css/main.a124c658.chunk.css"
+  },
+  {
+    "revision": "bcd8a54176861279615b",
+    "url": "/static/js/2.a4cb15d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4cb15d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a542c064a8c8c7c7d5",
+    "url": "/static/js/main.d8120496.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
