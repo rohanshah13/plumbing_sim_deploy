8e03ef5c213a72ab81604dee0c4476d444af1687
Rohan Shah <rohanshah1306@gmail.com>
2020-06-27 14:57:12 +0530

    logging3

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls import url
 
 urlpatterns = [
+	path('sim/', include('sim.urls')),
     path('admin/', admin.site.urls),
     url(r'^', include('sim.urls'))
 ]
--- sim/views.py
+++ sim/views.py
@@ -3,6 +3,9 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django.conf import settings
 import os
+import csv
+from sim.models import Game
+from postgres_copy import CopyManager
 # Create your views here.
 
 class ReactAppView(View):
@@ -17,3 +20,20 @@ class ReactAppView(View):
 				''',
 				status = 501,
 			)
+
+def index(request):
+	# Create the HttpResponse object with the appropriate CSV header.
+	file_path = './data.csv'
+	Game.objects.to_csv(file_path)
+	if os.path.exists(file_path):
+		with open(file_path, 'rb') as fh:
+			response = HttpResponse(fh.read(), content_type='text/csv')
+			response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+			return response
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+	writer = csv.writer(response)
+	writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+	writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+	return response
