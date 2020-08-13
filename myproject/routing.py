62d03f063ba0cc7022b83a2221cac454ca91f19e
Rohan Shah <rohanshah1306@gmail.com>
2020-08-13 12:07:13 +0530

    minor bug

--- myproject/routing.py
+++ myproject/routing.py
@@ -1,6 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import re_path
+from django.urls import re_path,path
 from sim import consumers
 from django.conf.urls import url
 
